661f42748b89e8b1b9ebd30678129d6df3b25452
Fan Wenbo <wbfan@thoughtworks.com>
2015-06-17 20:06:03 +0800

    create fixture for test data

---
+++ server/fixtures.jsx
@@ -0,0 +1,35 @@
+if (Meteor.users.find().count() == 0) {
+    console.log('Creating fixture...');
+    var bossId = Accounts.createUser({
+        username: 'USER_UUID',
+        email: 'sxj@waiterfree.com',
+        password: 'DEFAULT',
+        profile: {
+            name: '司女神',
+            type: 'boss',
+            shops: []
+        }
+
+    });
+    var shopId = Shops.insert({
+        name: '美食天下',
+        address: '锦业一路软件园H座16层',
+        desc: '集天下美食',
+        tags: ['中餐', '印度菜', '西餐', '土耳其菜'],
+        tel: '029-88886686',
+        status: 'open',
+        orders: [],
+        tables: [],
+        menu: {
+            dishes: []
+        },
+        boss: bossId,
+        clerks: []
+    });
+
+    Meteor.users.update(bossId, {
+        $push: {
+            'profile.shops': Shops.findOne(shopId)
+        }
+    });
+}
