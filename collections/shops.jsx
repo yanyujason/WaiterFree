44b1b16a1db429f546e8fcadfa53aeba46aa2173
Msk86 <msk86.kaiser@gmail.com>
2015-06-21 13:17:06 +0800

    remove inscure package, no Collections.insert|update|remove method can be executed from browser

--- collections/shops.jsx
+++ collections/shops.jsx
@@ -1,1 +1,13 @@
 Shops = new Meteor.Collection('shops');
+
+Shops.allow({
+    update(userId, shop) {
+        return shop && shop.boss === userId;
+    }
+});
+
+Shops.deny({
+    update(userId, shop, detailFields) {
+        return _.intersection(detailFields, ['status', 'boss', 'createdAt']).length;
+    }
+});
