d6284f19e37550c116429cef5f84a66dfe91c0b6
Fan Wenbo <wbfan@thoughtworks.com>
2015-06-25 17:41:04 +0800

    add full log and cache on travis, this maybe helpful for debug

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#export VELOCITY_DEBUG=1
-#export JASMINE_LOG_LEVEL=debug
-#export LONG_RUNNING_CHILD_PROCESS_LOG_LEVEL=debug
-#export VELOCITY_USE_CHECKED_OUT_METEOR=1
+export VELOCITY_DEBUG=1
+export JASMINE_LOG_LEVEL=debug
+export LONG_RUNNING_CHILD_PROCESS_LOG_LEVEL=debug
+export VELOCITY_USE_CHECKED_OUT_METEOR=1
 
 export JASMINE_BROWSER=Firefox
 export JASMINE_PACKAGES_TO_INCLUDE_IN_UNIT_TESTS=package-to-include
 
-meteor --test --once --release velocity:METEOR@1.1.0.2_3
+meteor --test --once --verbose --release velocity:METEOR@1.1.0.2_3
