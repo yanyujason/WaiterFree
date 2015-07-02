f89617c067cac60e574b8e12e492a5beca562d13
Fan Wenbo <wbfan@thoughtworks.com>
2015-07-02 19:08:36 +0800

    add link to clerk list, new clerk, clerk update.

--- lib/router.jsx
+++ lib/router.jsx
@@ -83,8 +83,9 @@ Router.route('/my-shops/:shopId/dishes/:dishId', function () {
 });
 
 Router.route('/my-shops/:shopId/clerks', function() {
-    this.layout('myShopLayout', {data: Shops.findOne({_id: this.params.shopId})});
-    this.render('clerkList', {data: {clerks: Meteor.users.find({'profile.shop': this.params.shopId, 'profile.type': 'clerk'})}});
+    var shopId = this.params.shopId;
+    this.layout('myShopLayout', {data: Shops.findOne({_id: shopId})});
+    this.render('clerkList', {data: {shopId: shopId, clerks: Meteor.users.find({'profile.shop': shopId, 'profile.type': 'clerk'})}});
 }, {
     name: 'clerkList',
     waitOn() {
