369ecd6f15c5b7ebf88130e5766808abcde4b3f3
Fan Wenbo <wbfan@thoughtworks.com>
2015-07-05 14:07:09 +0800

    add alert popup and confirm popup

--- client/lib/popup.jsx
+++ client/lib/popup.jsx
@@ -15,8 +15,55 @@ Popups.remove = () => {
     Blaze.remove(Popups.currentView);
 };
 
+Popups.alert = (options, callback) => {
+    if(typeof options === 'string') {
+        options = {message: options};
+    }
+    options.buttonText = options.buttonText || 'OK';
+    Popups.show('alertPopup', {
+        message: options.message,
+        buttonText: options.buttonText,
+        callback: callback
+    });
+};
+
+Popups.confirm = (options, callback) => {
+    if(typeof options === 'string') {
+        options = {message: options};
+    }
+    options.buttonText = options.buttonText || 'OK';
+    options.cancelButtonText = options.cancelButtonText || 'Cancel';
+    Popups.show('confirmPopup', {
+        message: options.message,
+        buttonText: options.buttonText,
+        cancelButtonText: options.cancelButtonText,
+        callback: callback
+    });
+};
+
 Template.popup.events({
     'click .popup-close': function() {
         Popups.remove();
+    }
+});
+
+Template.alertPopup.events({
+    'click .alert-popup .btn-confirm': function() {
+        Popups.remove();
+        this.callback();
+    }
+});
+
+Template.confirmPopup.events({
+    'click .confirm-popup .btn-confirm': function() {
+        Popups.remove();
+        this.callback(true);
+    }
+});
+
+Template.confirmPopup.events({
+    'click .confirm-popup .btn-cancel': function() {
+        Popups.remove();
+        this.callback(false);
     }
 });
